3c2ea4b8a48dfb981f6bd23705f119bcdddf807e
wiktorpp <grazynapp65@gmail.com>
2021-04-07 19:10:12 +0200

    Cleanup

--- pebblesay.py
+++ pebblesay.py
@@ -78,7 +78,7 @@ width = 35
 
 
 
-text = document["input"].value.split("\n")
+text = document["input"].value.split("<br>")
 think = False
 
 
@@ -99,59 +99,54 @@ width = max(len(i) for i in text)
 ####################
 #printing (finally)#
 ####################
+output = ""
 
 #printing top line
-document["output"].attach(" _".replace(" ", "\u2800"))
+output += " _"
 for i in range(0, width):
-    document["output"].attach('_')
-document["output"].attach("_")
+    output += '_'
+output += "_"
 
 #printing one line of text
 if len(text) == 1:
-    document["output"].attach("   {}".format(asciiart[0]).replace(" ", "\u2800"))
-    document["output"].attach(html.BR())
-    document["output"].attach("< {} >  {}".format(text[0], asciiart[1]).replace(" ", "\u2800"))
-    document["output"].attach(html.BR())
+    output += "   {}<br>".format(asciiart[0])
+    output += "< {} >  {}<br>".format(text[0], asciiart[1])
 
 #printing multiple lines of text
 elif len(text) > 1:
-    document["output"].attach("")
-    document["output"].attach(html.BR())
+    output += "<br>"
     for i in range(0, len(text)):
         if i == 0:
-            document["output"].attach("/ {} \\  ".format(text[0].ljust(width)).replace(" ", "\u2800"))
+            output += "/ {} \\  ".format(text[0].ljust(width))
         elif len(text) - i == 1:
-            document["output"].attach("\\ {} /  ".format(text[i].ljust(width)).replace(" ", "\u2800"))
+            output += "\\ {} /  ".format(text[i].ljust(width))
         else:
-            document["output"].attach("│ {} │  ".format(text[i].ljust(width)).replace(" ", "\u2800"))
+            output += "│ {} │  ".format(text[i].ljust(width))
 
         #printing asciiart
         if len(text) - i == 2:
-            document["output"].attach(asciiart[0].replace(" ", "\u2800"))
+            output += asciiart[0]
         elif len(text) - i == 1:
-            document["output"].attach(asciiart[1].replace(" ", "\u2800"))
+            output += asciiart[1]
 
-        document["output"].attach("")
-        document["output"].attach(html.BR())
+        output += "<br>"
 
 spacing = " " * width
 
 #printing bottom line
-document["output"].attach(" ¯".replace(" ", "\u2800"))
+output += " ¯"
 for i in range(0, width):
-    document["output"].attach('¯'.replace(" ", "\u2800"))
-document["output"].attach("¯ ".replace(" ", "\u2800"))
+    output += '¯'
+output += "¯ "
 if not think:
-    document["output"].attach("\\ {}".format(asciiart[2]).replace(" ", "\u2800"))
-    document["output"].attach(html.BR())
-    document["output"].attach((spacing + "     \\{}".format(asciiart[3])).replace(" ", "\u2800"))
-    document["output"].attach(html.BR())
+    output += "\\ {}<br>".format(asciiart[2])
+    output += spacing + "     \\{}<br>".format(asciiart[3])
 else:
-    document["output"].attach("o {}".format(asciiart[2]).replace(" ", "\u2800"))
-    document["output"].attach((spacing + "     o{}".format(asciiart[3])).replace(" ", "\u2800"))
-    document["output"].attach(html.BR())
+    output += "o {}<br>".format(asciiart[2])
+    output += spacing + "     o{}<br>".format(asciiart[3])
 
 #printing rest of asciiart
 for i in range(4, len(asciiart)):
-    document["output"].attach((spacing + asciiart[i] + "").replace(" ", "\u2800"))
-    document["output"].attach(html.BR())
+    output += spacing + asciiart[i] + "<br>"
+
+document["output"].attach(html.CODE(output.replace(" ", "\u2800")))
